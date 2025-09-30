168926d8751d0d5841295ad707a87457e0499269
Alexandre Bouchard-Doylee <boudoy03@gmail.com>
2025-09-30 16:09:58 -0400

    Added while loop to game day4_final

--- day4_final.py
+++ day4_final.py
@@ -26,19 +26,20 @@ scissors = """
 """
 options = [rock, paper, scissors]
 
-choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+while True: # infinite loop - not part of day 4 lessons ctrl + c to exit
+    choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
 
-if choice >= 3 or choice < 0:
-    print("Invalid choice. You lose!")
-else:
-    print("You chose: \n", options[choice])
+    if choice >= 3 or choice < 0:
+        print("Invalid choice. You lose!")
+    else:
+        print("You chose: \n", options[choice])
 
-computer = random.randint(0, 2)
-print("Computer chose:\n", options[computer])
+    computer = random.randint(0, 2)
+    print("Computer chose:\n", options[computer])
 
-if choice == computer:
-    print("It's a tie!")
-elif (choice == 0 and computer == 2) or (choice == 1 and computer == 0) or (choice == 2 and computer == 1):
-    print("You win!")
-else:
-    print("You lose!")
+    if choice == computer:
+        print("It's a tie!")
+    elif (choice == 0 and computer == 2) or (choice == 1 and computer == 0) or (choice == 2 and computer == 1):
+        print("You win!")
+    else:
+        print("You lose!")
