68c9307cf68a0a2a132b1cc2ca4dcd641abc1dd3
Alexandre Bouchard-Doylee <boudoy03@gmail.com>
2025-09-30 16:04:29 -0400

    Modified da4_final

--- day4_final.py
+++ day4_final.py
@@ -24,34 +24,21 @@ scissors = """
       (____)
 ---.__(___)
 """
-
-choice = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n")
-if choice == 0:
-    print(rock)
-elif choice == 1:
-    print(paper)
-else:
-    print(scissors)
-
-print("Computer chose:\n")
 options = [rock, paper, scissors]
-computer = random.choice(options)
-if computer == rock:
-    print(rock)
-elif computer == paper:
-    print(paper)
-else:
-    print(scissors)
 
-if choice == 0 and computer == scissors:
+choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+
+if choice >= 3 or choice < 0:
+    print("Invalid choice. You lose!")
+else:
+    print("You chose: \n", options[choice])
+
+computer = random.randint(0, 2)
+print("Computer chose:\n", options[computer])
+
+if choice == computer:
+    print("It's a tie!")
+elif (choice == 0 and computer == 2) or (choice == 1 and computer == 0) or (choice == 2 and computer == 1):
     print("You win!")
-elif choice == 0 and computer != scissors:
-    print("You lose!")
-if choice == 1 and computer == rock:
-    print("You win!")
-elif choice == 1 and computer != rock:
-    print("You lose!")
-if choice == 2 and computer == paper:
-    print("You win!")
-elif choice == 2 and computer != paper:
+else:
     print("You lose!")
